776f0768ae428eb33409ecf012c3fd54372bf78f
Archana <archanata207@gmail.com>
2026-06-02 06:28:19 +0530

    Added Canvas Structure and display functions

--- graphics_editor.c
+++ graphics_editor.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+typedef struct{
+    int rows;
+    int cols;
+    char pixels[50][100];
+}Canvas;
+
+void initializeCanvas(Canvas *c);
+void displayCanvas(Canvas *c);
+
 int main(){
     printf("2D Graphics Editor Project\n");
-    
+
+    Canvas picture;
+
+    picture.rows=20;
+    picture.cols=40;
+
+    initializeCanvas(&picture);
+    printf("\n===== 2D GRAPHICS EDITOR =====\n\n");
+    displayCanvas(&picture);
+
     return 0;
+}
+void initializeCanvas(Canvas *c){
+    int i,j;
+    for(i=0;i < c->rows;i++){
+        for(j=0;j <c->cols;j++){
+            c->pixels[i][j]='_';
+        }
+    }
+}
+void displayCanvas(Canvas *c){
+    int i,j;
+    for(i=0;i <c->rows;i++){
+        for(j=0;j <c->cols;j++){
+            printf("%c", c->pixels[i][j]);
+        }
+        printf("\n");
+    }
 }
