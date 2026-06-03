9ef719679319eca437f397fdef1c44dca11000ac
Archana <archanata207@gmail.com>
2026-06-03 21:34:05 +0530

    Day 4: Added line drawing function

--- graphics_editor.c
+++ graphics_editor.c
@@ -8,6 +8,7 @@ typedef struct{
 void initializeCanvas(Canvas *c);
 void displayCanvas(Canvas *c);
 void drawRectangle(Canvas *c,int r,int col,int h,int w);
+void drawLine(Canvas *c,int r1,int c1,int r2,int c2);
 
 int main(){
     printf("2D Graphics Editor Project\n");
@@ -16,10 +17,17 @@ int main(){
 
     picture.rows=20;
     picture.cols=40;
-
+    
     initializeCanvas(&picture);
+
     drawRectangle(&picture,3,5,10,20);
+
+    drawLine(&picture,1,2,1,30);
+
+    drawLine(&picture,0,35,15,35);
+
     printf("\n===== 2D GRAPHICS EDITOR =====\n\n");
+
     displayCanvas(&picture);
 
     return 0;
@@ -48,6 +56,29 @@ void drawRectangle(Canvas *c,int r,int col,int h,int w){
             if(i==r||i==r+h-1||j==col||j==col+w-1){
                 c->pixels[i][j]='*';
             }
+        }
+    }
+}
+void drawLine(Canvas *c,int r1,int c1,int r2,int c2){
+    int i;
+    if(r1==r2){
+        if(c1>c2){
+            int temp=c1;
+            c1=c2;
+            c2=temp;
+        }
+        for(i=c1;i<=c2;i++){
+            c->pixels[r1][i]='*';
+        }
+    }
+    else if(c1==c2){
+        if(r1>r2){
+            int temp=r1;
+            r1=r2;
+            r2=temp;
+        }
+        for(i=r1;i<=r2;i++){
+            c->pixels[i][c1]='*';
         }
     }
 }
