9cd9f1006dff8e2acb87c097e9de82630206a289
Archana <archanata207@gmail.com>
2026-06-07 09:52:55 +0530

    Day 8 : Added drawing character modification feature

--- graphics_editor.c
+++ graphics_editor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+char drawChar='*';
 typedef struct{
     int rows;
     int cols;
@@ -11,6 +12,7 @@ void drawRectangle(Canvas *c,int r,int col,int h,int w);
 void drawLine(Canvas *c,int r1,int c1,int r2,int c2);
 void drawTriangle(Canvas *c,int topRow,int topCol,int height);
 void drawCircle(Canvas *c,int centerRow,int centerCol,int radius);
+int isValidPoint(Canvas *c,int row,int col);
 
 int main(){
     printf("2D Graphics Editor Project\n");
@@ -31,7 +33,9 @@ int main(){
         printf("3. Add Triangle\n");
         printf("4. Add Circle\n");
         printf("5. Display Picture\n");
-        printf("6. Exit\n");
+        printf("6. Clear Canvas\n");
+        printf("7. Change Drawing Character\n");
+        printf("8. Exit\n");
 
         printf("Enter choice: ");
         scanf("%d",&choice);
@@ -42,7 +46,16 @@ int main(){
             int r,col,h,w;
             printf("Enter row column height width: ");
             scanf("%d%d%d%d",&r,&col,&h,&w);
-            drawRectangle(&picture,r,col,h,w);
+            if(!isValidPoint(&picture,r,col)){
+                printf("Invalid coordinates!\n");
+            }
+            else if(r+h>picture.rows||col+w>picture.cols){
+                printf("Shape exceeds canvas boundary!\n");
+            }
+            else{
+                drawRectangle(&picture,r,col,h,w);
+            }
+            displayCanvas(&picture);
             break;
         }
         case 2:{
@@ -51,21 +64,47 @@ int main(){
             scanf("%d%d",&r1,&c1);
             printf("Enter end row and column: ");
             scanf("%d%d",&r2,&c2);
-            drawLine(&picture,r1,c1,r2,c2);
+            if(isValidPoint(&picture,r1,c1)&&isValidPoint(&picture,r2,c2)){
+                drawLine(&picture,r1,c1,r2,c2);
+            }
+            else{
+                printf("Invalid coordinates!\n");
+            }
+            displayCanvas(&picture);
             break;
         }
         case 3:{
             int topRow,topCol,height;
             printf("Enter top row,top column,height: ");
             scanf("%d%d%d",&topRow,&topCol,&height);
-            drawTriangle(&picture,topRow,topCol,height);
+            if(!isValidPoint(&picture,topRow,topCol)){
+                printf("Invalid coordinates!\n");
+            }
+            else if(topRow+height>=picture.rows||
+             topCol-height+1<0||topCol+height-1>=picture.cols){
+                    printf("Triangle exceeds canvas boundary!\n");
+                }
+            else{
+                drawTriangle(&picture,topRow,topCol,height);
+            }
+            displayCanvas(&picture);
             break;
         }
         case 4:{
             int centerRow,centerCol,radius;
             printf("Enter center row,center column,radius: ");
             scanf("%d%d%d",&centerRow,&centerCol,&radius);
-            drawCircle(&picture,centerRow,centerCol,radius);
+            if(!isValidPoint(&picture,centerRow,centerCol)){
+                printf("Invalid coordinates!\n");
+            }
+            else if(centerRow-radius<0||centerRow+radius>=picture.rows||
+                centerCol-radius<0||centerCol+radius>=picture.cols){
+                    printf("Circle exceeds canvas boundary!\n");
+                }
+            else{
+                drawCircle(&picture,centerRow,centerCol,radius);
+            }
+            displayCanvas(&picture);
             break;
         }
         case 5:{
@@ -73,13 +112,24 @@ int main(){
             break;
         }
         case 6:{
+            initializeCanvas(&picture);
+            printf("Canvas cleared successfully.\n");
+            break;
+        }
+        case 7:{
+            printf("Enter new drawing character: ");
+            scanf(" %c",&drawChar);
+            printf("Drawing character changed to %c\n",drawChar);
+            break;
+        }
+        case 8:{
             printf("Exiting...\n");
             break;
         }
         default:
             printf("Invalid choice!\n");
         }
-    }while(choice!=6);
+    }while(choice!=8);
 
     return 0;
 }
@@ -100,12 +150,15 @@ void displayCanvas(Canvas *c){
         printf("\n");
     }
 }
+int isValidPoint(Canvas *c,int row,int col){
+    return row>=0 && row<c->rows && col>=0 && col<c->cols;
+}
 void drawRectangle(Canvas *c,int r,int col,int h,int w){
     int i,j;
     for(i=r;i < r+h;i++){
         for(j=col;j < col+w;j++){
             if(i==r||i==r+h-1||j==col||j==col+w-1){
-                c->pixels[i][j]='*';
+                c->pixels[i][j]=drawChar;
             }
         }
     }
@@ -119,7 +172,7 @@ void drawLine(Canvas *c,int r1,int c1,int r2,int c2){
             c2=temp;
         }
         for(i=c1;i<=c2;i++){
-            c->pixels[r1][i]='*';
+            c->pixels[r1][i]=drawChar;
         }
     }
     else if(c1==c2){
@@ -129,7 +182,7 @@ void drawLine(Canvas *c,int r1,int c1,int r2,int c2){
             r2=temp;
         }
         for(i=r1;i<=r2;i++){
-            c->pixels[i][c1]='*';
+            c->pixels[i][c1]=drawChar;
         }
     }
     else{
@@ -149,7 +202,7 @@ void drawLine(Canvas *c,int r1,int c1,int r2,int c2){
             colStep=-1;
         }
         while(1){
-            c->pixels[row][col]='*';
+            c->pixels[row][col]=drawChar;
             if(row==r2 && col==c2){
                 break;
             }
@@ -177,7 +230,7 @@ void drawCircle(Canvas *c,int centerRow,int centerCol,int radius){
             (col-centerCol)*(col-centerCol);
             if(distanceSquared>=radiusSquared-radius && 
                 distanceSquared<=radiusSquared+radius){
-                    c->pixels[row][col]='*';
+                    c->pixels[row][col]=drawChar;
             }
         }
     }
